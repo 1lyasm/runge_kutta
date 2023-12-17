cd99d2b35de52f651c1827edf588906d03899a39
1lyasm <il.mystafa@gmail.com>
2023-12-17 18:22:21 +0300

    add De

--- src/main.c
+++ src/main.c
@@ -4,6 +4,11 @@
 
 typedef enum { Debug, Normal, Error } Mode;
 
+typedef struct {
+    int *coefs;
+    int n;
+} De;
+
 static void debug(Mode mode, const char *fmt, ...) {
     if (mode == Debug) {
         va_list args;
@@ -16,6 +21,14 @@ static void debug(Mode mode, const char *fmt, ...) {
 static void fail(char *msg) {
     fprintf(stderr, "%s\n", msg);
     exit(EXIT_FAILURE);
+}
+
+static void *fmalloc(unsigned n) {
+    void *allocated = malloc(n);
+    if (allocated == NULL) {
+        fail("fmalloc: malloc failed");
+    }
+    return allocated;
 }
 
 static Mode initMode() {
@@ -38,16 +51,46 @@ static Mode initMode() {
     return mode;
 }
 
-static int takeInt(char *msg, Mode mode) {
+static int takeInt(Mode mode, char *fmt, ...) {
     int inp;
-    printf("%s", msg);
+    va_list args;
+    va_start(args, fmt);
+    vprintf(fmt, args);
     scanf(" %d", &inp);
     debug(mode, "takeInt: inp: %d\n", inp);
+    va_end(args);
     return inp;
+}
+
+static De *initDe(Mode mode) {
+    int i;
+    De *de = fmalloc(sizeof(De));
+    de->n = takeInt(mode, "Enter n: ");
+    de->coefs = fmalloc((unsigned)de->n * sizeof(int));
+    for (i = 0; i < de->n; ++i) {
+        de->coefs[i] = takeInt(mode, "Enter coefficient %d: ", i);
+    }
+    return de;
+}
+
+static void debugDe(De *de, Mode mode) {
+    int i;
+    debug(mode, "debugDe: de: \n");
+    debug(mode, "\tn: %d\n", de->n);
+    for (i = 0; i < de->n; ++i) {
+        debug(mode, "\tcoefs[%d]: %d\n", i, de->coefs[i]);
+    }
+}
+
+static void freeDe(De *de) {
+    free(de->coefs);
+    free(de);
 }
 
 int main() {
     Mode mode = initMode();
-    int n = takeInt("Enter n: ", mode);
+    De *de = initDe(mode);
+    debugDe(de, mode);
+    freeDe(de);
     return 0;
 }
